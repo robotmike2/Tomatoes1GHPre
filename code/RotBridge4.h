a159dbfa5f84fd510b2f6271a258dfbccb116300
mwinter <robotmikew@gmail.com>
2015-02-08 12:53:56 -0800

    file add
    file added

---
+++ code/RotBridge4.h
@@ -0,0 +1,17 @@
+//
+//  RotBridge4.h
+//  code1RotTomatoes
+//
+//  Created by Michael Winter on 2/6/15.
+//  Copyright (c) 2015 com.codepath. All rights reserved.
+//
+
+
+
+#ifndef RotBridge4
+#define RotBridge4
+
+#import "AFNetworking.h"
+//#import "JGProgressHUD/JGProgressHUD.h"
+
+#endif
